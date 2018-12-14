f7c60b767f57793f4ef4821e882aaf1425ee2970
Erno Lokkila <eolokk@utu.fi>
2018-12-14 02:26:45 +0200

    2015 day 14

---
+++ code2015/py3/day14/day14_2.py
@@ -0,0 +1,44 @@
+#Vixen can fly 19 km/s for 7 seconds, but then must rest for 124 seconds
+class Raindeer:
+
+    def __init__(self, name, speed, time, rest):
+        
+        self.name = name
+        self.speed = speed
+        self.time = time
+        self.rest = rest
+        self.ableToFly = time
+        self.mustRestFor = 0
+        self.distance = 0
+        self.score = 0
+
+    def fly(self):        
+        if self.ableToFly > 0:
+            self.ableToFly -= 1
+            self.distance += self.speed
+            if self.ableToFly == 0:
+                self.mustRestFor = self.rest
+        else:
+            self.mustRestFor -= 1
+            if self.mustRestFor == 0:
+                self.ableToFly = self.time
+
+    def giveScore(self):
+        self.score += 1
+        
+raindeer = []
+with open("../../data/14.txt","r") as f:
+    #f = ["Comet can fly 14 km/s for 10 seconds, but then must rest for 127 seconds.","Dancer can fly 16 km/s for 11 seconds, but then must rest for 162 seconds."]
+    for line in f:
+        split = line.split(" ")
+        raindeer.append(Raindeer(split[0],int(split[3]), int(split[6]), int(split[13])))
+
+seconds = 2503
+for i in range(seconds):    
+    for rd in raindeer:        
+        rd.fly()
+
+    [rd.giveScore() for rd in raindeer if rd.distance == sorted(raindeer, key = lambda a : a.distance, reverse = True)[0].distance]
+        
+raindeer.sort(key = lambda a : a.score, reverse = True)
+print (raindeer[0].score)
