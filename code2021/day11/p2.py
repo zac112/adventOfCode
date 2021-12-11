56e0877575270e3e45a9bcea04a1374b9af98b7a
Erno Lokkila <eolokk@utu.fi>
2021-12-11 13:04:14 +0200

    AoC21 day 11

---
+++ code2021/day11/p2.py
@@ -0,0 +1,49 @@
+with open('data.txt') as file:
+    dumbos = [[int(x) for x in line[:-1]] for line in file.readlines()]
+
+print(dumbos)
+
+def neighborIndices(matrix, y,x,size):
+    for i in range(-size,size+1):
+        for j in range(-size,size+1):
+            x1, y1 = x+j, y+i
+            if i == 0 and j == 0:
+                continue
+            if x1 < 0 or y1 < 0 or x1 >=len(matrix[0]) or y1 >=len(matrix):
+                continue
+            yield (y1,x1)
+
+def findCoords(m, comp):
+    return [(x,y) for x in range(len(dumbos)) for y in range(len(dumbos[0])) if comp(dumbos[x][y])]
+
+def step():
+    flashed = set()
+    newDumbos = [[dumbos[i][j]+1 for j in range(len(dumbos[0]))] for i in range(len(dumbos))]
+    
+    def flash(i,j):
+        #print('flashing',i,j)
+        for i,j in neighborIndices(dumbos,i,j,size=1):            
+            newDumbos[i][j] += 1
+            dumbos[i][j] += 1
+    
+    while flashable := findCoords(dumbos, lambda a:a>=9):
+        for i,j in flashable:
+            if dumbos[i][j] >= 9:                         
+                flash(i,j)
+                flashed.add((i,j))
+
+        for i,j in flashed:
+            dumbos[i][j] = 0
+            newDumbos[i][j] = 0
+                
+    return newDumbos
+
+
+
+steps = 0
+while len(flashed := findCoords(dumbos, lambda a:a!=0))>0:
+    dumbos = step()
+    steps += 1
+
+print(steps)
+print(dumbos)
