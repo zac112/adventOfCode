d039cfc317d615046a4108d53e054672249f4e2d
Erno Lokkila <eolokk@utu.fi>
2023-12-06 07:46:16 +0200

    Day 6, 2023

---
+++ code2023/day6/p1.py
@@ -0,0 +1,19 @@
+from functools import reduce
+class Race:    
+    def __init__(self, time, distance) -> None:
+        self.time = time
+        self.distance = distance
+
+races = [Race(41,244), Race(66,1047), Race(72,1228), Race(66,1040)]
+
+def win(race):
+    waystoWin = 0
+    for t in range(race.time):
+        distance = (race.time-t)*t
+        if distance > race.distance:
+            waystoWin += 1
+    return waystoWin
+
+
+total = reduce(lambda a,b:a*b,map(win,races),1)
+print(total)
