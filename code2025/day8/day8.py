8b505afb9c1d8bacb45e5fc29386c8883cac0c83
Erno Lokkila <eolokk@utu.fi>
2025-12-08 23:48:44 +0200

    Optimized 2025 day 8

--- code2025/day8/day8.py
+++ code2025/day8/day8.py
@@ -10,31 +10,29 @@ for box in boxes:
     circuit = set()
     circuit.add(box)
     allCircuits.append(circuit)
-        
+
 def part1():
-    circuits = list(allCircuits)    
+    circuits = list(allCircuits)
+    distances = []
+    for i,b1 in enumerate(boxes):
+        for b2 in boxes[i+1:]:
+            if b1==b2:continue
+            distances.append((b1,b2,distance(*b1,*b2)))
+            
+    distances.sort(key=lambda a:a[2])
     mindist = 0
     
     for i in range(1000):
-        print(i)
-        closest = None
-        for b1 in boxes:
-            for b2 in boxes:
-                if b1==b2:continue
-                dist = distance(*b1,*b2)
-                if closest is None or (dist>mindist and dist < closest[0]):
-                    closest = (dist, b1, b2)
-
-        d,b1,b2 = closest
+        b1,b2,d = distances.pop(0)                     
         mindist = d
+        
         for circ in circuits:
             if b1 in circ: b1_c = circ
             if b2 in circ: b2_c = circ
         if b1_c == b2_c:continue
-        
+
         b1_c |= b2_c
         circuits.remove(b2_c)
-
         
     a,b,c = sorted(map(len,circuits),reverse=True)[:3]
     print("Part 1:", a*b*c)
